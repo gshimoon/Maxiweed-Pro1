2a70a2fd156cbfb5aa074e3aa36b126735618452
gshimoon <gabriel.shimoon@gmail.com>
2024-08-09 22:42:06 -0300

    This is a test

--- Maxiweed Pro/ContentView.swift
+++ Maxiweed Pro/ContentView.swift
@@ -19,6 +19,7 @@ struct ARViewContainer: UIViewRepresentable {
     func makeUIView(context: Context) -> ARView {
         
         let arView = ARView(frame: .zero)
+//addition as a test
         
         // Load the "Box" scene from the "Experience" Reality File
         let boxAnchor = try! Experience.loadBox()
